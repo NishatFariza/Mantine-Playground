2f992fbaeee0c64d5d649f95ef7e3287080fc50a
NishatFariza <nishatfarzia78@gmail.com>
2022-11-18 11:50:07 +0600

    tailwind error fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,40 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { NavbarMinimal } from '../components/Navbar'
 import styles from '../styles/Home.module.css'
+import { Button } from "@mantine/core";
 
 export default function Home() {
   return (
     <div>
-      <NavbarMinimal/>
-   </div>
-  )
+      <NavbarMinimal />
+
+      <div className="flex gap-4">
+        <h1>Hello</h1>
+        <Button variant="gradient" gradient={{ from: "indigo", to: "cyan" }}>
+          Indigo cyan
+        </Button>
+        <Button
+          variant="gradient"
+          gradient={{ from: "teal", to: "lime", deg: 105 }}
+        >
+          Lime green
+        </Button>
+        <Button
+          variant="gradient"
+          gradient={{ from: "teal", to: "blue", deg: 60 }}
+        >
+          Teal blue
+        </Button>
+        <Button variant="gradient" gradient={{ from: "orange", to: "red" }}>
+          Orange red
+        </Button>
+        <Button
+          variant="gradient"
+          gradient={{ from: "#ed6ea0", to: "#ec8c69", deg: 35 }}
+        >
+          Peach
+        </Button>
+      </div>
+    </div>
+  );
 }
