97305d3ef06d136d8c70c2c9f931fbd40c22d379
NishatFariza <nishatfarzia78@gmail.com>
2022-11-18 10:57:22 +0600

    wip:navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { NavbarMinimal } from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      
+      <NavbarMinimal/>
    </div>
   )
 }
